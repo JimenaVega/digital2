03345f6c1d38340dea54bf9d5ea21c32f142ea81
sebbk3 <63316781+sebbk3@users.noreply.github.com>
2020-06-08 21:34:52 -0300

    Add files via upload
    Solo falta TMR1,Teclado y Tx

--- TPFinal.X/TPFinal.asm
+++ TPFinal.X/TPFinal.asm
@@ -4,7 +4,7 @@ LIST P=16F887
  __CONFIG _CONFIG2, _BOR4V_BOR40V & _WRT_OFF
  
 ;Variables T0
- asd
+ 
 ;Variables T1
  TMR1Flag     equ 0x		    ;Flag para resolver interrupcion por T1 fuera
 ;Variables multiplexado
@@ -12,7 +12,11 @@ LIST P=16F887
 ;Variables teclado
  botonFlag    equ 0x		    ;Flag para resolver interrupcion por teclado fuera
 ;Variables sensor
- asd
+ varUnidades  equ 0x		    ;Para realizar calculos de distancia
+ varDecenas   equ 0x		    ;Los separo en unidades, decenas y centenas
+ varCentenas  equ 0x		    ;Para mostrarlos en 3 Display
+ distanciaL   equ 0x		    ;Se usara un contador durante la medicion
+ distanciaH   equ 0x		    ;De dos registros, porque la distancia maxima es 400cm
 ;Variables puerto serie
  asd
 ;Variables contexto
@@ -20,6 +24,7 @@ LIST P=16F887
  STATUS_TEMP  equ 0x		    ;Variables temporales para salvar contexto - STATUS
 ;Variables propositos generales
  estadoRB     equ 0x		    ;Variable que guarda estado anterior de puerto B
+ varAux	      equ 0x		    ;Variable para hacer calculos auxiliares
 
 
 INCLUDE <P16F887.INC> 
@@ -37,9 +42,15 @@ Inicio
  ;Configuracion inicial de registros auxiliares
  clrf	    TMR1Flag
  clrf	    botonFlag
+ clrf	    varUnidades
+ clrf	    varDecenas
+ clrf	    varCentenas
+ clrf	    distanciaL
+ clrf	    distanciaH
  clrf	    W_TEMP
  clrf	    STATUS_TEMP
  clrf	    estadoRB
+ clrf	    varAux
  ;Configuracion Puerto B
  BANKSEL    TRISB
     movlw	B'00010011'	    ;RB0,RB1 y RB4 entrada
@@ -148,13 +159,113 @@ intRB
  
 
 rutinaSensor 
- asd
+ btfss	    PORTB,RB4		    ;Pregunto si RB4=1
+    goto	$+9		    ;No, entonces termino medicion y salto 9 instrucciones
     
+ bsf	    estadoRB,4		    ;Si, empezo medicion
+ BANKSEL    OPTION_REG		    ;RBPU* INTEDG T0CS T0SE PSA PS2 PS1 PS0
+ movlw	    B'00000000'		    ;pull up,int rb0,fosc/4,alto a bajo,psv tmr0, psv=2
+ addwf	    OPTION_REG,F	    ;Por las pull up ya tiene valores que no quiero cambiar
+ BANKSEL    PORTA		    ;Vuelvo al banco 1
+ movlw	    D'240'		    ;59us=1cm, no hay el mismo tiempo inicial que en RESETT0
+ movwf	    TMR0
+ bsf	    INTCON,T0IE		    ;Habilito interrupciones por TMR0
+ bcf	    INTCON,RBIF		    ;Bajo bandera de interrupcion en Puerto B
+ clrf	    distanciaL
+ clrf	    distanciaH
+ return
+  
+ bcf	    estadoRB,4		    ;Termino medicion, y RB4=0
+ bcf	    INTCON,T0IE		    ;Deshabilito interrupcion por TMR0
+ call	    calculosDistancia	    ;Realizo los calculos de distancia para U,D y C
+ bcf	    INTCON,RBIF		    ;Bajo bandera de interrupcion en Puerto B
+ return
+    
+ 
 RESETT0
+ movlw	    D'235'		    ;Para 59us
+ movwf	    TMR0
+ incfsz	    distanciaL,F	    ;Aumento contador, si pasa de FFh a 00h salta
+    goto	$+2		    ;No se paso, salta a bajar la bandera
+ incf	    distanciaH,F	    ;Se paso de FFh, asi que subo banco alto
+ bcf	    INTCON,T0IF		    ;Bajo bandera de interrupcion por TMR0
+ return
+ 
+TECLADO
+ asd
+ 
+subrutinaTMR1
  asd
  
 rutinaTX
  asd
 
+calculosDistancia
+L1
+ movlw	    D'100'
+ subwf	    distanciaL,W	    ;Me fijo si le puedo restar 100 al banco bajo
+ btfss	    STATUS,C		    ;Si no se puede, resultado negativo y C = 0
+    goto	L2		    ;No se puede restar 100, voy a L2
+ movlw	    D'100'		    ;Si se puede
+ subwf	    distanciaL,F	    ;Asi que efectivamente le resto 100
+ incf	    varCentenas,F	    ;Y aumento las centenas
+ goto	    L1			    ;Y vuelvo a L1
+L2
+ movlw	    D'10'		    ;Ahora se que no puedo restar 100
+ subwf	    distanciaL,W	    ;Intento restarle 10
+ btfss	    STATUS,C
+    goto	L3		    ;No se puede, voy a L3
+ movlw	    D'10'		    ;Si se puede
+ subwf	    distanciaL,F	    ;Asi que efectivamente le resto 10
+ incf	    varDecenas,F	    ;Y aumento las decenas
+ subwf	    varDecenas,W	    ;Me fijo si ya tengo 10 decenas
+ btfss	    STATUS,Z		    ;Z = 1 si la resta da 0
+    goto	L2		    ;No tengo 10 decenas, vuelvo a L2
+ incf	    varCentenas,F	    ;Llegue a 10 decenas, entonces subo una centena
+ clrf	    varDecenas		    ;Y limpio las decenas
+ goto	    L2			    ;Finalmente vuelvo a L2
+L3
+ movlw	    D'1'		    ;Ahora se que no puedo restar 10
+ subwf	    distanciaL,W	    ;Intento restarle 1
+ btfss	    STATUS,C		    
+    goto	L4		    ;No se puede, voy a L4
+ movlw	    D'1'		    ;Si se puede
+ subwf	    distanciaL,F	    ;Asi que efectivamente le resto 1
+ incf	    varUnidades,F	    ;Y aumento las unidades
+ movlw	    D'10'		    
+ subwf	    varUnidades,W	    ;Me fijo si ya tengo 10 unidades
+ btfss	    STATUS,Z		    
+    goto	L3		    ;No tengo 10 unidades, asi que vuelvo a L3
+ incf	    varDecenas,F	    ;Llegue a 10 unidades, asi que subo una decena
+ clrf	    varUnidades		    ;Limpio las unidades
+ movlw	    D'10'		    ;Y nuevamente me fijo si llegue a las 10 decenas
+ subwf	    varDecenas,W
+ btfss	    STATUS,Z
+    goto	L3
+ incf	    varCentenas,F
+ clrf	    varDecenas
+ goto	    L3
+L4
+ movlw	    D'1'		    ;En este punto el banco bajo esta vacio
+ subwf	    distanciaH,W	    ;Asi que me fijo si puedo restarle al banco alto
+ btfss	    STATUS,C
+    return			    ;Ambos bancos estan vacios, entonces termine y vuelvo
+ movlw	    D'1'		    ;Si puedo
+ subwf	    distanciaH,F	    ;Entonces efectivamente le resto 1
+ incf	    varUnidades,F	    ;Y porque resto 1, debo sumar una unidad
+ movlw	    D'10'		    ;Y al igual que antes debo ver si me paso 
+ subwf	    varUnidades,W	    ;De 10 unidades o centenas
+ btfss	    STATUS,Z
+    goto	L1		    ;Con la excepcion de que ahora vuelvo a L1
+ incf	    varDecenas,F
+ clrf	    varUnidades
+ movlw	    D'10'
+ subwf	    varDecenas,W
+ btfss	    STATUS,Z
+    goto	L1
+ incf	    varCentenas,F
+ clrf	    varDecenas
+ goto	    L1
+ 
  
 END
